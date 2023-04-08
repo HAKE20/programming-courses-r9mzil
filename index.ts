dcfe65451714e5e3318f4fcd59bf0e56d8a84bfb
HAKE20 <130192097+HAKE20@users.noreply.github.com>
2023-04-08 05:08:30 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -13,10 +13,4 @@ for (j = 0; j < 4; j++) {
   list[j] = [];
   list[i][j] = parseInt(prompt('nemr'));
 }
-for (i = 0; i < list.length; i++) {
-  console.log('list[' + i + ']=' + list[i]);
-}
-for (j = 0; j < list.length; j++) {
-  console.log('list[' + j + ']=' + list[j]);
-}
 console.log('list[' + i + j + ']=' + list[i][j]);
